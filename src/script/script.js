36b3714bf2b0267a7813279357dafbad58e8cd59
depstor <depstor90@gmail.com>
2025-09-16 18:07:18 +0300

    functiom-container-padding

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,14 @@
 import '/src/sass/style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    //!Header
+    const loadingElementByPages = url => {
+        fetch(url)
+            .then(res => res.text())
+            .then(html => {
+                document.getElementById('header').innerHTML = html
+            })
+    }
+
+    loadingElementByPages('/shared/header.html')
+})
